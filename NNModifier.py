83b4aae8d5e39e4249a1f02dbe32db464cdf4f4c
ssz <sad>
2019-02-24 10:54:31 +0100

    Modyfikacja NNModifyier
    Poprawienie nagłównów importujących na keras. Dodanie metody add_loss_layer_for_knowlage_distillation.

--- NNModifier.py
+++ NNModifier.py
@@ -1,8 +1,9 @@
 import tensorflow.keras as keras
 import tensorflow as tf
-from tensorflow.python.keras.layers import Input, Dense, MaxPool2D, Conv2D, Flatten, Dropout, Activation, Softmax
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.utils import plot_model
+from keras.layers import Input, Dense, MaxPool2D, Conv2D, Flatten, Dropout, Activation, Softmax, Lambda
+from keras.models import Model
+from keras.utils import plot_model
+from CreateNN import CreateNN
 class NNModifier:
 
 
@@ -48,7 +49,7 @@ class NNModifier:
         return Model(model.input, model.layers[layer].output)
 
     @staticmethod
-    def add_classifier_to_end(model, number_of_neurons=1000, number_of_classes=10):
+    def add_classifier_to_end(model, number_of_neurons=512, number_of_classes=10):
 
         y = model.output
         y = Flatten()(y)
@@ -63,22 +64,35 @@ class NNModifier:
         # Rozbicie sieci na warstey
         layers = [l for l in model.layers]
         model.summary()
-        # input = layers[0].output
-        x = layers[0].output
-        with_conv = 1  # Licznik warstw konwolucyjnych
+        input = layers[0].output
 
-        for i in range(1, len(layers)):
+        witch_conv = 1  # Licznik warstw konwolucyjnych
+
+        # dla pierwszej warstwy, zamiana input na x
+        if type(layers[1]) == Conv2D:  # Sprawdzenie czy warstwa jest konwolucyjna
+            if not (witch_conv in layers_to_remove):  # Jeżeli dana warstwa jest na liście to nie dodawaj jej do sieci
+                print(layers[1].input)
+                print(input.shape)
+                config = layers[1].get_config()
+                x = layers[1](input)  # Łączenie warstw sieci neuronowej
+            witch_conv += 1
+
+        for i in range(2, len(layers)):
             if type(layers[i]) == Conv2D:        # Sprawdzenie czy warstwa jest konwolucyjna
-                if not (with_conv in layers_to_remove):  # Jeżeli dana warstwa jest na liście to nie dodawaj jej do sieci
+                if not (witch_conv in layers_to_remove):  # Jeżeli dana warstwa jest na liście to nie dodawaj jej do sieci
+                    print('i:', i)
                     print(layers[i].input)
-                    print(x.shape)
+                    # print(x.shape)
                     config = layers[i].get_config()
                     x = layers[i](x)  # Łączenie warstw sieci neuronowej
-                with_conv += 1
+                witch_conv += 1
             else:
+                print('i:', i)
+                print(layers[i].input)
+                # print(input.shape)
                 x = layers[i](x)  # Łączenie warstw sieci neuronowej Jeżeli nie jest konwolucyjna
 
-        return Model(inputs=model.input, outputs=x)
+        return Model(inputs=input, outputs=x)
 
     @staticmethod
     def remove_last_layer(model):
@@ -92,4 +106,45 @@ class NNModifier:
             x = layers[i](x)  # Łączenie warstw sieci neuronowej
 
         return Model(model.input, x)
+
+
+
+    @staticmethod
+    def add_loss_layer_for_knowledge_distillation(model, alpha=0.5, T=1):
+        """Dodanie warstwy obliczającej loss sieci neuronowej. Warstwa dodawana jest na koniec sieci neuronowej.
+        Dodatkowo dodawane są dwa dodatkowe wejścia ground_truth, logits. Są to odpowiedzi sieci orginalnej oraz jej
+        wartości sprzed wejścia na softmax.
+
+        # Argumenty:
+        model- model sieci do której ma być dodana warstwa
+        alfa- parametr wyliczania lossu. Waga odpowiadająca za logitsy
+        T- temperatura Logitsów
+
+        Zwraza:
+        model sieci"""
+
+        ground_truth = Input(shape=10)
+        logits = Input(shape=10)
+
+        loss = Lambda(CreateNN.loss_for_knowledge_distillation(alpha=alpha, T=T), name='loss')([ground_truth, logits,
+                                                                                model.layers[-1].outputs,
+                                                                                model.layers[-2].outputs])
+
+        model = Model(model.inputs, loss)
+        return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

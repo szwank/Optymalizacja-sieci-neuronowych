db32879f19d801106b83506cb4c27d5bb497b1c6
szwank <sad>
2019-03-12 12:53:59 +0100

    Dodanie metody save_weights
    Metoda zapisuje wagi danej sieci.

--- NNSaver.py
+++ NNSaver.py
@@ -1,10 +1,21 @@
 import json
+import os
 
 class NNSaver:
 
     @staticmethod
-    def save(model, file_name):
+    def save_model(model, file_name):
         json_str = model.to_json()
         with open(file_name, 'w') as outfile:
                 json.dump(json_str, outfile)
+
+    @staticmethod
+    def save_weights(model, file_name, dir='temp'):
+        if not os.path.exists(dir):  # Stworzenie folderu jeżeli nie istnieje.
+            os.makedirs(dir)
+
+        if file_name in os.listdir(dir + '/'):  # Usunięcie schematu sieci jeżeli istnieje
+            os.remove(dir + '/' + file_name)
+
+        model.save_weights(dir + '/' + file_name)
 

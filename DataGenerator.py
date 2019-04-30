37a5117be762a138141bbbedf6a32be41a204b8d
szwank <sad>
2019-04-30 11:05:53 +0200

    Poprawki w nazewnictwie zmiennych DataGenerator

--- DataGenerator.py
+++ DataGenerator.py
@@ -12,18 +12,18 @@ class DataGenerator(keras.utils.Sequence):
         self.batch_size = batch_size
         # self.labels = labels
         # self.list_IDs = list_IDs
-        self.h5_file = h5py.File(data_dir, 'r')
-        self.n_channels = n_channels
-        self.n_classes = n_classes
+        self.h5_file_to_be_processed = h5py.File(data_dir, 'r')
+        self.number_of_channels_in_data = n_channels
+        self.number_of_classes = n_classes
         self.shuffle = shuffle
-        self.x_data_name = x_data_name
+        self.name_of_dataset_in_file = x_data_name
         self.y_data_name = y_data_name
-        self.indexes = np.arange(len(self.h5_file[x_data_name]))
+        self.indexes = np.arange(len(self.h5_file_to_be_processed[x_data_name]))
         self.on_epoch_end()
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.h5_file[self.x_data_name]) / self.batch_size))
+        return int(np.floor(len(self.h5_file_to_be_processed[self.name_of_dataset_in_file]) / self.batch_size))
 
     def __getitem__(self, index):
         """Generate one batch of data"""
@@ -46,15 +46,15 @@ class DataGenerator(keras.utils.Sequence):
     def __data_generation(self, indexes):
         """Generates data containing batch_size samples"""  # X : (n_samples, *dim, n_channels)
         # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size, self.n_classes), dtype=int)
+        X = np.empty((self.batch_size, *self.dim, self.number_of_channels_in_data))
+        y = np.empty((self.batch_size, self.number_of_classes), dtype=int)
 
         # Generate data
         for i, ID in enumerate(indexes):
             # Store sample
             # X[i,] = np.load('data/' + ID + '.npy')
-            X[i, ] = self.h5_file[self.x_data_name][ID]
+            X[i, ] = self.h5_file_to_be_processed[self.name_of_dataset_in_file][ID]
             # Store class
-            y[i, ] = self.h5_file[self.y_data_name][ID]
+            y[i, ] = self.h5_file_to_be_processed[self.y_data_name][ID]
 
         return X, y

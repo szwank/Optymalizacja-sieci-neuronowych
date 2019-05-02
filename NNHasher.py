b857eb2f3e706f480136af0cb0ca0c99f99e752c
szwank <sad>
2019-05-02 16:24:03 +0200

    poprawienie hashera sieci neurnowych

--- NNHasher.py
+++ NNHasher.py
@@ -1,6 +1,7 @@
 from NNLoader import NNLoader
 from NNModifier import NNModifier
-import xxhash
+import numpy as np
+
 
 class NNHasher:
 
@@ -8,7 +9,7 @@ class NNHasher:
     def hash_model(model):
         [x_train, x_validation, x_test], [y_train, y_validation, y_test] = NNLoader.load_CIFAR10()
         model = NNModifier.remove_last_layer(model)
-        prediction = model.predict(x_test[:10])
-        return xxhash.xxh64(prediction.tostring(), seed=0).hexdigest()
+        prediction = model.predict(x_test[0:1])
 
+        return np.array2string(np.dot(prediction, prediction.transpose()), precision=4)
 

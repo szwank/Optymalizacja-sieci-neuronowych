af810ba331a9f2b1cac67021829aa36f5add4220
szwank <sad>
2019-05-03 11:29:57 +0200

    poprawienie zapisywania CIFAR10 do h5py

--- utils/Convert_Cifar10_to_h5py.py
+++ utils/Convert_Cifar10_to_h5py.py
@@ -2,14 +2,14 @@ from NNLoader import NNLoader
 import h5py
 
 [x_train, x_validation, x_test], [y_train, y_validation, y_test] = NNLoader.load_CIFAR10()
-h5f = h5py.File('data/CIFAR10.h5', 'w')
-h5f.create_dataset('x_train', data=x_train)
-h5f.create_dataset('x_validation', data=x_validation)
-h5f.create_dataset('x_test', data=x_test)
+h5f = h5py.File('/home/szwank/Desktop/Optymalizacja-sieci-neuronowych/data/CIFAR10.h5', 'a')
+h5f.create_dataset('x_train', data=x_train, dtype='float64')
+h5f.create_dataset('x_validation', data=x_validation, dtype='float64')
+h5f.create_dataset('x_test', data=x_test, dtype='float64')
 
-h5f.create_dataset('y_train', data=y_train)
-h5f.create_dataset('y_validation', data=y_validation)
-h5f.create_dataset('y_test', data=y_test)
+h5f.create_dataset('y_train', data=y_train, dtype='float64')
+h5f.create_dataset('y_validation', data=y_validation, dtype='float64')
+h5f.create_dataset('y_test', data=y_test, dtype='float64')
 
 
 h5f.close()

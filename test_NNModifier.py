80eb2653001405a2df118fe7f7c2f9820d84641f
Szwank <lukpanki@gmail.com>
2019-06-28 11:42:44 +0200

    Dodanie testów do remove chosen filters

--- test_NNModifier.py
+++ test_NNModifier.py
@@ -2,9 +2,14 @@ import unittest
 
 from keras import Input, Model, backend as K
 from keras.layers import Lambda, Softmax
+from CreateNN import CreateNN
+import json
+import numpy as np
 
 from CreateNN import CreateNN
-import numpy as np
+from NNModifier import NNModifier
+from keras import backend as K
+
 
 
 class TestLossLayer(unittest.TestCase):
@@ -85,4 +90,41 @@ class TestSoftSoftMax(unittest.TestCase):
                             'Data: {}\n Tested: {}\n Correct: {}'.format(x, model_tested.predict(x, batch_size=2), model.predict(x, batch_size=2)))
 
 
+class TestRemoveChosenFiltersFromModel(unittest.TestCase):
 
+    def remove_filters_and_test(self, number_of_filters_to_remove: list):
+        model = CreateNN.create_VGG16_for_CIFAR10()
+        number_of_filters_in_network = [64, 64, 128, 128, 256, 256, 256, 512, 512, 512, 512, 512, 512]
+
+        filters_to_remove = {}
+        for i, element in enumerate(number_of_filters_to_remove):
+                filters_to_remove.update({i+1: np.arange(1, element+1)})
+
+        model = NNModifier.remove_chosen_filters_from_model(model, filters_to_remove, 1, debug=True)
+        model_dictionary = json.loads(model.to_json())
+
+        conv_layer_counter = 0
+        for layer in model_dictionary['config']['layers']:
+            if 'conv' in layer['name']:
+                conv_layer_counter += 1
+                number_of_filters_in_actual_conv_layer = number_of_filters_in_network[conv_layer_counter-1] -\
+                                                         number_of_filters_to_remove[conv_layer_counter-1]
+                self.assertEqual(layer['config']['filters'], number_of_filters_in_actual_conv_layer)
+
+    def test_removing_filters(self):
+
+        number_of_filters_to_remove = [63, 63, 127, 127, 255, 255, 255, 511, 511, 511, 511, 511, 511]
+        self.remove_filters_and_test(number_of_filters_to_remove)
+        K.clear_session()
+
+        number_of_filters_to_remove = [63, 0, 0, 0, 255, 255, 0, 0, 511, 0, 0, 511, 511]
+        self.remove_filters_and_test(number_of_filters_to_remove)
+        K.clear_session()
+
+        number_of_filters_to_remove = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.remove_filters_and_test(number_of_filters_to_remove)
+        K.clear_session()
+
+        number_of_filters_to_remove = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        self.remove_filters_and_test(number_of_filters_to_remove)
+        K.clear_session()

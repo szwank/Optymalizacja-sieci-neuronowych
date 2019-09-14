e7df93ee9d0c0a7eec8390f85bce03676ce7a3f1
szwank <lukpankiewicz@gmail.com>
2019-09-14 15:02:01 +0200

    removed bugs from get_results_from_networkA

--- get_results_from_networkA.py
+++ get_results_from_networkA.py
@@ -35,19 +35,24 @@ def safe_list_gate(list, index):
         return None
 
 
+
 def main():
     results = {}
 
-    for fold in range(1, 6):        # fold
-        path = os.path.join('NetworkA', 'fold' + str(fold))
+    for fold in range(1):        # fold
+        path = os.path.join('Zapis modelu')
         folders_in_directory = get_folders_in_directory(path)
 
-        fold_results = {}
+        type_of_optymalization = {}
 
-        for folder in folders_in_directory:     # filrst firectory(type of optymalization)
+        for folder in folders_in_directory:     # filrst directory(type of optymalization)
+            if folder not in ['shallowed_model_removing_filters', 'shallowed_model_removing_random_filters', 'shallowed_model_removing_whole_layers']:
+                continue
+
             folders_in_folder = get_folders_in_directory(os.path.join(path, folder))
+            type_of_optymalization_results = {}
 
-            for folder_with_files in folders_in_folder:     # second folder farameters of optymalization
+            for folder_with_files in folders_in_folder:     # second folder parameters of optymalization
 
                 numbers_in_folder_name = get_numbers_from_string(folder_with_files, splited_by="_")
                 result = {}
@@ -61,10 +66,11 @@ def main():
                     accuracy = i / (i+1) * accuracy + get_numbers_from_string(file[:-5], "_")[0] / (i+1)
                     result['test_accuracy'] = accuracy
 
-                fold_results["".join([str(remove_if_below), '/', str(leave_if_above)])] = result
-        results[fold] = fold_results
+                type_of_optymalization_results["".join([str(remove_if_below), '/', str(leave_if_above)])] = result
+            type_of_optymalization[folder] = type_of_optymalization_results
+        results[fold] = type_of_optymalization
 
-    file = open('optymalization_results.txt', 'a+')
+    file = open('optymalization_results-cifar10.txt', 'a+')
     results = json.dumps(results)
     file.write(results)
     file.close()

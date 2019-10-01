e5dca4225356f6e468e4e76d0ea6dfde262d05e5
Szwank <lukpanki@gmail.com>
2019-10-01 11:52:44 +0200

    removed optymalization structure code from files

--- assesing_VGG16_clasyfying_CIFAR10.py
+++ assesing_VGG16_clasyfying_CIFAR10.py
@@ -52,7 +52,6 @@ def get_list_of_files_in_directory(path):
 def main():
     train_whole_layers = False
     train_filters = False
-    optimize_networks = False
 
     if train_whole_layers is True:
         path = 'Zapis modelu/VGG16-CIFAR10-0.94acc.hdf5'
@@ -80,50 +79,6 @@ def main():
                               filters_in_grup_after_division=1,
                               resume_testing=False)
 
-
-
-    if optimize_networks is True:
-
-        path = 'Zapis modelu/VGG16-CIFAR10-0.94acc.hdf5'
-
-        model = load_model(path)
-        model_dict = NNModifier.convert_model_to_dictionary(model)
-        model_hash = NNHasher.hash_model(model)
-        K.clear_session()
-
-        path_to_assesing_file_full_layers = model_hash
-        path_to_assesing_file_single_filters = model_hash + 'v2'
-
-        integrity_result = check_integrity_of_score_file(path_to_assesing_file_single_filters, model_dict)
-
-        if integrity_result:
-            print("Score file for single filters is correct.")
-        else:
-            raise (ValueError("File {} have a bug. Result in layers {} aren't correct".format(
-                path_to_assesing_file_single_filters, integrity_result)))
-
-        shallow_model = shallow_network_based_on_filters(path, path_to_assesing_file_single_filters,
-                                                         path_to_assesing_file_full_layers)
-
-        path_to_shallowed_model = 'temp/shallowed_model.hdf5'
-
-        save_model(shallow_model, path_to_shallowed_model)
-
-        K.clear_session()
-
-        generators_for_training = get_generators_for_training(zbior)
-
-        shallowed_model = knowledge_distillation(path_to_shallowed_model, path, generators_for_training)
-
-        optimizer_SGD = SGD(lr=0.01, momentum=0.9, nesterov=True)
-        shallowed_model.compile(optimizer=optimizer_SGD, loss='binary_crossentropy', metrics=['accuracy'])
-        test_generator = generators_for_training.get_test_data_generator_flow(batch_size=128, shuffle=True)
-        scores = shallowed_model.evaluate_generator(test_generator, steps=len(test_generator))
-
-        shallowed_model_name = "".join(
-            ['Shallowed_model_number_of_parameters_', str(shallowed_model.count_params()), '_test_accuracy_', str(scores[1]), '.hdf5'])
-        path_to_shallowed_model = os.path.join(path, 'shallowed_model', shallowed_model_name)
-        NNSaver.save_model(shallowed_model, path_to_shallowed_model)
 
 
 if __name__ == '__main__':
